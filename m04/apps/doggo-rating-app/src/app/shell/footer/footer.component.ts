39fc2d4bf4fde71c95323508c3f75442800546a2
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-03-19 18:37:32 +0100

    improved footer

--- m04/apps/doggo-rating-app/src/app/shell/footer/footer.component.ts
+++ m04/apps/doggo-rating-app/src/app/shell/footer/footer.component.ts
@@ -1,10 +1,5 @@
 import { AsyncPipe, NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
-import { environment } from './../../../environments/environment';
-import { selectCurrentUserIdentifier } from './../../auth/store/auth.selectors';
-import { getRealTimeConnection } from './../../doggos/store/doggos.selectors';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -13,17 +8,7 @@ import { getRealTimeConnection } from './../../doggos/store/doggos.selectors';
   styleUrls: ['./footer.component.css'],
   imports: [AsyncPipe, NgIf],
 })
-export class FooterComponent implements OnInit {
-  userEmail$: Observable<string>;
-
-  backendUrl = environment.server;
-
-  realTimeConnection$: Observable<string>;
-
-  constructor(private store: Store) {}
-
-  ngOnInit(): void {
-    this.userEmail$ = this.store.pipe(select(selectCurrentUserIdentifier));
-    this.realTimeConnection$ = this.store.pipe(select(getRealTimeConnection));
-  }
+export class FooterComponent {
+  @Input() userEmail = '';
+  @Input() backendUrl = '';
 }
