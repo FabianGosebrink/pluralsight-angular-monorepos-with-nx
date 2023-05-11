4aebdaf4835e111e132f926be4c118abbacf240b
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-05-11 10:03:25 +0200

    initial commit

---
+++ m04/live/doggo-rating-workspace/libs/doggos/domain/src/lib/models/doggo.ts
@@ -0,0 +1,11 @@
+export interface Doggo {
+  id: string;
+  name: string;
+  breed: string;
+  comment: string;
+  imageUrl: string;
+  ratingSum: number;
+  ratingCount: number;
+  created: Date;
+  userId: string;
+}
