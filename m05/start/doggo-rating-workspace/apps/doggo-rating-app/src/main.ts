68ea2996f8731dd7b2e3d522f01ce05cd7f02c1c
FabianGosebrink <fabian.gosebrink@outlook.com>
2023-05-21 17:56:18 +0200

    adding m05

---
+++ m05/start/doggo-rating-workspace/apps/doggo-rating-app/src/main.ts
@@ -0,0 +1,63 @@
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideRouter } from '@angular/router';
+import { EffectsModule } from '@ngrx/effects';
+import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AuthEffects, authReducer } from '@ps-doggo-rating/shared/util-auth';
+import { environment } from '@ps-doggo-rating/shared/util-environments';
+import { AuthInterceptor, AuthModule } from 'angular-auth-oidc-client';
+import { ToastrModule } from 'ngx-toastr';
+import { APP_ROUTES } from './app/app-routes';
+import { AppComponent } from './app/app.component';
+
+const webCallbackUrl = `${window.location.origin}/callback`;
+
+console.log('Running with endpoint', environment.server);
+
+if (environment.production) {
+  enableProdMode();
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    provideRouter(APP_ROUTES),
+    provideHttpClient(),
+    importProvidersFrom(
+      StoreModule.forRoot({
+        router: routerReducer,
+        auth: authReducer,
+      }),
+      EffectsModule.forRoot([AuthEffects]),
+      StoreRouterConnectingModule.forRoot(),
+      StoreDevtoolsModule.instrument({
+        maxAge: 25,
+        logOnly: environment.production,
+      }),
+      AuthModule.forRoot({
+        config: {
+          authority: 'https://dev-2fwvrhka.us.auth0.com',
+          redirectUrl: webCallbackUrl,
+          clientId: 'W6a2DDLMzlWPF6vZ5AKKNnFVonklSU0m',
+          scope: 'openid profile email offline_access access:api',
+          responseType: 'code',
+          silentRenew: true,
+          useRefreshToken: true,
+          postLogoutRedirectUri: window.location.origin,
+          customParamsAuthRequest: {
+            audience: environment.server,
+          },
+          secureRoutes: [environment.server],
+        },
+      }),
+      ToastrModule.forRoot({
+        positionClass: 'toast-bottom-right',
+      }),
+      BrowserAnimationsModule
+    ),
+  ],
+});
---
+++ m05/start/doggo-rating-workspace/libs/buildable-test/README.md
@@ -0,0 +1,7 @@
+# buildable-test
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test buildable-test` to execute the unit tests.
